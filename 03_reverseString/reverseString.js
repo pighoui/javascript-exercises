01ae7d3316bfa5877c2a90f0aad973b764f2b37c
Ethan <111640712+pighoui@users.noreply.github.com>
2023-08-08 12:49:03 +1000

    Complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let letterArray = string.split("");
+    let reverse = '';
+    for (let index = letterArray.length - 1; index >= 0; index--) {
+        reverse += letterArray[index];
+    }
+    return reverse;
 };
 
 // Do not edit below this line
