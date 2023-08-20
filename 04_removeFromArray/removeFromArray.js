6ac02cb9e92cdf7e3691160e6bf7bd2965b86447
Ethan <111640712+pighoui@users.noreply.github.com>
2023-08-20 20:15:40 +1000

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,24 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removed) {
 
+    const newArray = [];
+    
+    for (let arrayElement of array) {
+
+        for (let removedElement of removed) {
+            
+            if (arrayElement === removedElement) {
+                console.log('same');
+                break;
+            } 
+            
+            // Once all removed elements have been checked, push arrayElement to newArray
+            else if (removedElement === removed[removed.length - 1]) {
+                newArray.push(arrayElement);
+            }
+        }
+    }
+    
+    return newArray;
 };
 
 // Do not edit below this line
