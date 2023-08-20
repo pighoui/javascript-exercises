cd3abc1e5419e38d5ea41c9d10109754bc9a68fa
Ethan <111640712+pighoui@users.noreply.github.com>
2023-08-20 20:51:25 +1000

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,22 @@
-const sumAll = function() {
+const sumAll = function(number1, number2) {
+    
+    let sum = 0;
+
+    if (typeof(number1) != "number" || typeof(number2) != "number" || number1 < 0 || number2 < 0) {
+        return 'ERROR'
+    }
+
+    if (number1 <= number2) {
+        for (let i = number1; i <= number2; i++) {
+            sum += i;
+        }
+    } else {
+        for (let i = number2; i <= number1; i++) {
+            sum += i;
+        }
+    }
+
+    return sum;
 
 };
 
