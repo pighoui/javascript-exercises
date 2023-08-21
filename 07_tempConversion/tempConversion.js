a8b6aa97eb5a6db4c0c872176a17fa0258dc879b
Ethan <111640712+pighoui@users.noreply.github.com>
2023-08-21 13:24:53 +1000

    Complete 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celsius = (fahrenheit - 32) / 1.8;
+  //return celsius.toFixed(1);
+  return Math.round(celsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenheit = 1.8 * celsius + 32;
+  return Math.round(fahrenheit * 10) / 10;
 };
 
 // Do not edit below this line
