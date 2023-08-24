fbe447c8eaaf4a1e0616c5e37cf6db37fda9dbe9
Ethan <111640712+pighoui@users.noreply.github.com>
2023-08-25 08:41:29 +1000

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    // removes all punctuation and decapitalises letters from original string
+    string = string.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()\s]/g, ""); 
+    
+    // reverse of original string
+    let reverseString = string.split("").reverse().join("");
+    
+    // returns true if they are the same
+    return reverseString == string;
 };
 
 // Do not edit below this line
