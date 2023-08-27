78fb1af72bd48bbf87522217b1e0d6a85d692065
Ethan <111640712+pighoui@users.noreply.github.com>
2023-08-28 08:26:57 +1000

    Complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(fibIndex) {
+    
+    // Array to store fibonacci series
+    let fibSeries = [1, 1];
+    
+    if (Number(fibIndex) < 0) {
+        return 'OOPS'
+    }
+    
+    // Until there are enough fibonacci numbers, push the sum of the last and second last numbers
+    while (fibSeries.length < Number(fibIndex)) {
+        fibSeries.push( fibSeries[ fibSeries.length - 1 ] + fibSeries[ fibSeries.length - 2 ] )
+    }
+    
+    return fibSeries[Number(fibIndex) - 1]
 };
 
 // Do not edit below this line
